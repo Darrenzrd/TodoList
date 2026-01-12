049b891c6931776262f7fc72fc53880cc2da3a8c
Darrenzrd <zangruida@icloud.com>
2026-01-12 22:33:58 +0800

    Fist TodoList project submmit, the project can links to 8080 and output HelloController

---
+++ src/main/java/com/darren/todolist/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.darren.todolist.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello Spring Boot";
+    }
+}
