b7b9059d39c756ca08ea52ac251da0642cff65fb
Darrenzrd <zangruida@icloud.com>
2026-02-01 17:13:33 +0800

    新增返回体

---
+++ src/main/java/com/darren/todolist/dto/Result.java
@@ -0,0 +1,22 @@
+package com.darren.todolist.dto;
+
+public class Result <T>{
+    private int code;
+    private String msg;
+    private T data;
+
+    public static <T> Result<T> success(T data){
+        Result<T> r = new Result<>();
+        r.code = 0;
+        r.msg = "success";
+        r.data = data;
+        return r;
+    }
+
+    public static <T> Result<T> fail(String msg) {
+        Result<T> r = new Result<>();
+        r.code = -1;
+        r.msg = msg;
+        return r;
+    }
+}
